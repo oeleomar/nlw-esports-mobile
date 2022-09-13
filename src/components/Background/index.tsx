55a86ad704da8cecf7d7f5d735a78e3090bc75e9
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-09-13 13:28:06 -0400

    Abb background component

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,7 @@
+import { ImageBackground } from 'react-native';
+
+import { styles } from './styles';
+
+export function Background() {
+  return <ImageBackground style={styles.container}></ImageBackground>;
+}
